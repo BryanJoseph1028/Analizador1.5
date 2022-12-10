47d29751826ed5ff65ae0f44ded7b8de90baf413
bryan <bryancorado777.com>
2022-12-09 20:20:58 -0600

    first commit

---
+++ analexico 4/src/analexico/Token.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analexico;
+
+/**
+ *
+ * @author Jhensser
+ */
+public enum Token {
+    igual,
+    suma, 
+    Identificador,
+    Numero, 
+    multiplicacion, 
+    resta, 
+    division, 
+    menque,
+    mayque,
+    diferencia, 
+    menIgualQue,
+    mayorIgualQue,
+    parentesisAp,
+    parentesisCie,
+    llaveAp, 
+    llaveCie,
+    dospuntos,
+    PyComa,
+    IgualQue,
+    coma,
+    SaltoDeLinea,
+    Simbolo, 
+    Espacio,
+    PALABRA_RESERVADA,
+    Operador,
+    Comilla,
+    Cadena, 
+    ERROR,
+    ID,
+    guionbaj;   
+}
